dff0410515b991dcf4ab0985116b6fddffe00b56
PraveenS2022 <praveens.7349@gmail.com>
2025-04-23 17:24:57 +0530

    youtube web page

---
+++ JavaScript/demo.js
@@ -0,0 +1,23 @@
+var age =10
+let gender='male'
+const institute='x-workz'
+
+let emp1={id:12,employeeName:'aa',salary:10000}
+
+let emp2= new Object();
+emp2.id=13;
+emp2.employeeName='bb';
+emp2.salary=50000;
+emp2.id=14;
+
+function emp3(id,employeeName,salary){
+    this.id=id;
+    this.employee=this.employee;
+    this.salary=salary;
+}
+
+console.log(age,gender,institute)
+console.log(emp1)
+
+ 
+
